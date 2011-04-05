4f8ea8667a7ed67ca86b585bcf28d5c8053862d0
superpea <scott@plexapp.com>
2011-04-05 15:23:58 -0700

    If we don't find an album match on last.fm, add a null match so that the rest of the metadata agents get called. Whitespace cleanup.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -11,17 +11,15 @@ class LastFmAgent(Agent.Artist):
   languages = [Locale.Language.English]
   
   def search(self, results, media, lang):
-    
     score = 100
     for r in lastfm.SearchArtists(String.StripDiacritics(media.artist))[0]:
       id = r[0]
       if id.find('+noredirect') == -1:
         id = r[1]
         id = String.Quote(id.encode('utf-8'))
-        Log('id: ' + id + '  name: '+ r[1] + '   score: ' + str(score) + '   thumb: ' + str(r[2]))
+        #Log('id: ' + id + '  name: '+ r[1] + '   score: ' + str(score) + '   thumb: ' + str(r[2]))
         results.Append(MetadataSearchResult(id = id.replace('%2B','%20'), name  = r[1], thumb = r[2], lang  = lang, score = score))
         score = score - 2
-    
     # Finally, de-dupe the results.
     toWhack = []
     resultMap = {}
@@ -30,27 +28,22 @@ class LastFmAgent(Agent.Artist):
         resultMap[result.id] = True
       else:
         toWhack.append(result)
-
     for dupe in toWhack:
       results.Remove(dupe)
 
   def update(self, metadata, media, lang):
     artist = XML.ElementFromURL(lastfm.ARTIST_INFO % String.Quote(String.Unquote(metadata.id), True))[0]
     summary = artist.xpath('//bio/content')[0]
-
     metadata.title = String.Unquote(artist.xpath('//artist/name')[0].text, True)
-    print "TITLE:", metadata.title
-    
+    #print "TITLE:", metadata.title
     if summary.text:
       metadata.summary = self.decodeXml(re.sub(r'<[^<>]+>', '', summary.text))
-
     try:
       url = artist.xpath('//artist/image[@size="mega"]//text()')[0]
       if url not in metadata.posters:
         metadata.posters[url] = Proxy.Media(HTTP.Request(url))
     except:
       pass
-      
     metadata.genres.clear()
     for genre in artist.xpath('//artist/tags/tag/name'):
       metadata.genres.add(genre.text.capitalize())
@@ -66,19 +59,25 @@ class LastFmAlbumAgent(Agent.Album):
   languages = [Locale.Language.English] 
   
   def search(self, results, media, lang):
+    highScore = 100
     for album in lastfm.ArtistAlbums(String.Unquote(media.parent_metadata.id)):
       (name, artist, thumb, url) = album
       albumID = url.split('/')[-1]
       id = media.parent_metadata.id + '/' + albumID.replace('+', '%20')
       dist = Util.LevenshteinDistance(name, media.album)
+      Log('media.album: ' + media.album) 
+      Log('last.fm album name: ' + name + ' [Distance = ' + str(dist) + ']')
       results.Append(MetadataSearchResult(id = id, name = name, thumb = thumb, lang  = lang, score = 90-dist))
+      highScore = 90 - dist
+    if highScore < 85:
+      #add a null album just so the secondary agents still get called.
+      results.Append(MetadataSearchResult(id = 'null', score = 100))
     results.Sort('score', descending=True)
  
   def update(self, metadata, media, lang):
     (artistName, albumName) = metadata.id.split('/')
     artistName = String.Unquote(artistName).encode('utf-8')
     albumName = String.Unquote(albumName).encode('utf-8')
-
     album = XML.ElementFromURL(lastfm.ALBUM_INFO % (String.Quote(artistName, True), String.Quote(albumName, True)))
     thumb = album.xpath("//image[@size='extralarge']")[0].text
     metadata.title = album.xpath("//name")[0].text
@@ -88,8 +87,6 @@ class LastFmAlbumAgent(Agent.Album):
       metadata.originally_available_at = Datetime.ParseDate(date).date()
     if thumb not in metadata.posters:
       metadata.posters[thumb] = Proxy.Media(HTTP.Request(thumb))
-    
     tracks = lastfm.AlbumTrackList(artistName, albumName)
     for num in range(len(tracks)):
       metadata.tracks[str(num+1)].name = tracks[num][0]
-      
