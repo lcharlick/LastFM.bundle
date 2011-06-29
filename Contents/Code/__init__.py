82ee33dc4b6e4726d56f4f3f9b90b04663f09023
superpea <scott@plexapp.com>
2011-06-28 23:36:31 -0700

    short circuiting for [unknown artist/album] and Various Artists. start of album scoring for Artists--to confirm artist selection.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -23,12 +23,18 @@ class LastFmAgent(Agent.Artist):
   
   def search(self, results, media, lang):
     score = 100
+    if media.artist == '[Unknown Artist]': return
+    if media.artist == 'Various Artists':
+      results.Append(MetadataSearchResult(id = 'Various%20Artists', name= 'Various Artists', thumb = 'http://userserve-ak.last.fm/serve/252/46209667.png', lang  = lang, score = 100))
+      return
     for r in lastfm.SearchArtists(self.safe_strip(media.artist))[0]:
       id = r[0]
       if id.find('+noredirect') == -1:
         id = r[1]
+        albumScore = self.checkArtistMatchUsingAlbums(media, id)
+        Log('artist: ' + media.artist + ' albumScore: ' + str(albumScore))
         id = String.Quote(id.encode('utf-8'))
-        Log('id: ' + id + '  name: '+ r[1] + '   score: ' + str(score) + '   thumb: ' + str(r[2]))
+        Log('Artist result: id: ' + id + '  name: '+ r[1] + '   score: ' + str(score) + '   thumb: ' + str(r[2]))
         results.Append(MetadataSearchResult(id = id.replace('%2B','%20'), name  = r[1], thumb = r[2], lang  = lang, score = score))
         score = score - 2
     
@@ -40,26 +46,38 @@ class LastFmAgent(Agent.Artist):
         resultMap[result.id] = True
       else:
         toWhack.append(result)
-
     for dupe in toWhack:
       results.Remove(dupe)
-
+  
+  def checkArtistMatchUsingAlbums(self, media, artistID):
+    lastFM_artistAlbums = []
+    for album in lastfm.ArtistAlbums(artistID):
+      (name, artist, thumb, url) = album
+      lastFM_artistAlbums.append(name.lower())
+    if len(lastFM_artistAlbums) == 0: return 100 #no last.fm albums for the artist, so abort!
+    solidMatchCount = 0
+    for a in media.children:
+      artist = a.title.lower()
+      for lfa in lastFM_artistAlbums:
+        score = Util.LevenshteinDistance(lfa, artist)
+        if score <= 2: 
+          Log('solid match!')
+          solidMatchCount += 1
+    return solidMatchCount
+    
   def update(self, metadata, media, lang):
     artist = XML.ElementFromURL(lastfm.ARTIST_INFO % String.Quote(String.Unquote(metadata.id), True))[0]
+    albumScore = self.checkArtistMatchUsingAlbums(media, '11000')
     summary = artist.xpath('//bio/content')[0]
-
     metadata.title = String.Unquote(artist.xpath('//artist/name')[0].text, True)
-    
     if summary.text:
       metadata.summary = self.decodeXml(re.sub(r'<[^<>]+>', '', summary.text))
-
     try:
       url = artist.xpath('//artist/image[@size="mega"]//text()')[0]
       if url not in metadata.posters:
         metadata.posters[url] = Proxy.Media(HTTP.Request(url))
     except:
-      pass
-      
+      pass     
     metadata.genres.clear()
     for genre in artist.xpath('//artist/tags/tag/name'):
       metadata.genres.add(genre.text.capitalize())
@@ -75,6 +93,8 @@ class LastFmAlbumAgent(Agent.Album):
   languages = [Locale.Language.English]
   fallback_agent = 'com.plexapp.agents.allmusic'
   def search(self, results, media, lang):
+    Log('album search')
+    if media.parent_metadata.id == '[Unknown Album]': return #eventually, we might be able to look at tracks to match the album
     if media.parent_metadata.id != 'Various%20Artists': 
       for album in lastfm.ArtistAlbums(String.Unquote(media.parent_metadata.id)):
         (name, artist, thumb, url) = album
@@ -92,8 +112,14 @@ class LastFmAlbumAgent(Agent.Album):
           dist = Util.LevenshteinDistance(name, media.album)
           results.Append(MetadataSearchResult(id = id, name = name, thumb = thumb, lang  = lang, score = 85-dist))
     results.Sort('score', descending=True)
+
+  def checkAlbumMatchUsingTracks(self, media, albumID):
+    lastfm.fetchAlbumTracks(albumID)
+    score = 0
+    return score
  
   def update(self, metadata, media, lang):
+    Log('album update')
     (artistName, albumName) = metadata.id.split('/')
     artistName = String.Unquote(artistName).encode('utf-8')
     albumName = String.Unquote(albumName).encode('utf-8')
